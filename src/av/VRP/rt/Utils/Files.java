f870054f76ed41ddcde2781cf690286c3f5c8a54
artbigtema <belfegor732@gmail.com>
2016-04-21 18:44:52 +0400

    Класс для работы с ФС.

---
+++ src/av/VRP/rt/Utils/Files.java
@@ -0,0 +1,43 @@
+package av.VRP.rt.Utils;
+
+import java.io.*;
+
+/**
+ * Created by Artem on 21.04.2016.
+ */
+public class Files {
+
+    public static PrintWriter getWriter(String s, int n) throws FileNotFoundException, UnsupportedEncodingException {
+        return new PrintWriter(Constant.DIR + "/" + Utils.getTitle(s) + n + Constant.FILE_FORMAT, Constant.FILE_ENCODING);
+    }
+
+    public static FileReader getReader(int i) throws IOException {
+        return new FileReader(getFileInDir(Constant.DIR, i));
+    }
+
+    public static void createDirectory() {
+        new File(Constant.DIR).mkdir();
+    }
+
+    public static boolean deleteDirectory() {
+        return deleteDirectory(new File(Constant.DIR));
+    }
+
+    public static boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                files[i].delete();
+            }
+        }
+        return path.delete();
+    }
+
+    public static String getFileInDir(String dir, int i) throws IOException {
+        return new File(Constant.DIR).listFiles()[i].getCanonicalPath();
+    }
+
+    public static int getCountFilesInDir(String dir) {
+        return new File(Constant.DIR).listFiles().length;
+    }
+}
